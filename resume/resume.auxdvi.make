ac80d9033c8158eb38fb49124015e8885b9f11a0
Mark Keele <mkeele@indiana.edu>
2014-12-15 02:05:23 -0500

    initial commit

---
+++ resume/resume.auxdvi.make
@@ -0,0 +1,1 @@
+\newlabel{LastPage}
---
+++ resume/resume.d
@@ -0,0 +1,18 @@
+# vim: ft=make
+.PHONY: resume._graphics
+resume.aux resume.aux.make resume.d resume.dvi: /usr/local/texlive/2012/texmf-dist/tex/generic/ifxetex/ifxetex.sty
+resume.aux resume.aux.make resume.d resume.dvi: /usr/local/texlive/2012/texmf-dist/tex/generic/oberdiek/gettitlestring.sty
+resume.aux resume.aux.make resume.d resume.dvi: /usr/local/texlive/2012/texmf-dist/tex/generic/oberdiek/hobsub-generic.sty
+resume.aux resume.aux.make resume.d resume.dvi: /usr/local/texlive/2012/texmf-dist/tex/generic/oberdiek/hobsub-hyperref.sty
+resume.aux resume.aux.make resume.d resume.dvi: /usr/local/texlive/2012/texmf-dist/tex/latex/base/article.cls
+resume.aux resume.aux.make resume.d resume.dvi: /usr/local/texlive/2012/texmf-dist/tex/latex/base/latexsym.sty
+resume.aux resume.aux.make resume.d resume.dvi: /usr/local/texlive/2012/texmf-dist/tex/latex/fancyhdr/fancyhdr.sty
+resume.aux resume.aux.make resume.d resume.dvi: /usr/local/texlive/2012/texmf-dist/tex/latex/geometry/geometry.sty
+resume.aux resume.aux.make resume.d resume.dvi: /usr/local/texlive/2012/texmf-dist/tex/latex/graphics/keyval.sty
+resume.aux resume.aux.make resume.d resume.dvi: /usr/local/texlive/2012/texmf-dist/tex/latex/hyperref/hyperref.sty
+resume.aux resume.aux.make resume.d resume.dvi: /usr/local/texlive/2012/texmf-dist/tex/latex/hyperref/nameref.sty
+resume.aux resume.aux.make resume.d resume.dvi: /usr/local/texlive/2012/texmf-dist/tex/latex/lastpage/lastpage.sty
+resume.aux resume.aux.make resume.d resume.dvi: /usr/local/texlive/2012/texmf-dist/tex/latex/oberdiek/kvoptions.sty
+resume.aux resume.aux.make resume.d resume.dvi: /usr/local/texlive/2012/texmf-dist/tex/latex/oberdiek/rerunfilecheck.sty
+resume.aux resume.aux.make resume.d resume.dvi: /usr/local/texlive/2012/texmf-dist/tex/latex/url/url.sty
+resume.aux resume.aux.make resume.d resume.dvi: resume.tex
